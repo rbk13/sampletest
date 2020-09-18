a468170272776260491e2b0d516b9cce200b9791
rbk13 <67997475+rbk13@users.noreply.github.com>
2020-09-18 21:19:23 +0530

    Update main.h

--- minip/head/main.h
+++ minip/head/main.h
@@ -1,1 +1,17 @@
 
+#ifndef __ADD_H__
+#define __ADD_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 + operand2
+*/
+int add(int operand1, int operand2);
+
+#endif  
